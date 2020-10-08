1bd473e30b8d0be98ed4c5820fd4fe08907a9e73
Thomas <thomas.j.purdie@hotmail.com>
2020-10-08 13:08:56 +1300

    Add project files.

---
+++ BusStopDocs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Web;
+
+//Icon:
+//      Send Document by Antonio Vicién Faure from the Noun Project
+
+namespace BusStopDocs
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string argument = String.Join(" ", args);
+            Console.WriteLine(argument);
+            string file = HttpUtility.UrlDecode(argument.Split(":")[1].Replace("/", "\\"));
+            Console.WriteLine(file);
+
+
+
+            new Process
+            {
+                StartInfo = new ProcessStartInfo(file)
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
+
+        }
+    }
+}
